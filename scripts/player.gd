4ef86cb8fa7484aeca6fd312ef340fb910166194
Kayque Estevão <kayque2911@gmail.com>
2026-05-10 21:52:54 -0300

    feat: ajustando pulo player 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,11 +7,18 @@ const gravity = 900
 @export var input_left: String
 @export var input_right: String
 @export var input_jump: String
+@export var input_attack: String
+
+var health = 100
+
+@onready var attack_area = $AttackArea
 
 func _physics_process(delta: float) -> void:
 	move(delta)
+	attack()
 
 func move(delta):
+
 	# Gravidade
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -23,7 +30,32 @@ func move(delta):
 	# Pulo
 	if Input.is_action_just_pressed(input_jump) and is_on_floor():
 		velocity.y = jump_force
-	up_direction = Vector2.UP 
-	
 
 	move_and_slide()
+
+func attack():
+
+	if Input.is_action_just_pressed(input_attack):
+
+		print(name, "ATACOU")
+
+		for body in attack_area.get_overlapping_bodies():
+
+			if body != self and body.has_method("take_damage"):
+
+				body.take_damage(10)
+
+func take_damage(damage):
+
+	health -= damage
+
+	print(name, " VIDA:", health)
+
+	if health <= 0:
+		die()
+
+func die():
+
+	print(name, " MORREU")
+
+	queue_free()
